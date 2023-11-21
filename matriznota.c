27722182406355b17bacd0371f6a4419c3e358d4
leoohh <144128024+leoohh@users.noreply.github.com>
2023-11-21 10:50:25 -0300

    Add files via upload

--- matriznota.c
+++ matriznota.c
@@ -9,7 +9,9 @@ int main()
     char alunos [2] [200];
     float notas [2] [3];
     int i,j;
-
+    float media [3];
+    int soma = 0;
+    
 
     for ( i = 0; i < 2; i++){
       fflush (stdin);
@@ -21,9 +23,12 @@ int main()
     {
      printf ("digite aq nota do aluno:");
      scanf("%f", &notas [i] [j]);
+     soma += notas [i] [j];
 
 
     }
+     media [i] = soma / j;
+     soma = 0;
 
 }
 
@@ -32,16 +37,19 @@ int main()
     for ( i = 0; i < 2; i++){
 
         printf ("Nome do aluno: %s\n", alunos[i]);
+          printf ("Media: %.1f\n", media [i]);
 
         for ( j = 0; j < 3; j++)
         {
           printf ("Nota dos alunos: %.1f\n", notas [i] [j]);
+
         }
         
 
 
     }
-    
+
+  
 
     return 0;
 }
